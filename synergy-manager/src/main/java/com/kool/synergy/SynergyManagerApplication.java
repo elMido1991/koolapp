cb5fd3e380225fd73c4c6d4d24b6d3f1877e4a16
El Mehdi AZOUI <mehdi.azoui@gmail.com>
2020-06-23 09:26:38 +0100

    test

--- synergy-manager/src/main/java/com/kool/synergy/SynergyManagerApplication.java
+++ synergy-manager/src/main/java/com/kool/synergy/SynergyManagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SynergyManagerApplication {
-
+//TODO
 	public static void main(String[] args) {
 		SpringApplication.run(SynergyManagerApplication.class, args);
 	}
